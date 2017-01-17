4f62e1a387e81e7aba9c08ba6fa426c798d61c60
Jeff Ding <dingjeff3@gmail.com>
2017-01-17 18:45:53 -0500

    moved parseTracks from client to server

--- server/methods.js
+++ server/methods.js
@@ -105,6 +105,53 @@ Meteor.methods({
 
 });
 
+// parse raw text tracks info into array object
+function parseTracks(text) {
+  // turn into list of invididual tracks
+  var stripped = text.replace(/["]+/g, '');
+  var textList = stripped.split('\\\\').slice(1, -1);
+
+  // return array of objects
+  var soundtrack = [];
+
+  for (var i in textList) {
+    // seperate fields of each track
+    var trackInfo = textList[i].split("\\");
+    var numTracks = trackInfo.length;
+
+    // initialize track object with title
+    var track = {};
+    track.title = trackInfo[0].trim();
+
+    // parse rest of fields
+    for (var j = 1; j < numTracks; j++) {
+      if (trackInfo[j].match(/.*Performed .*by /) !== null) {
+        var performer = trackInfo[j].replace(/.*Performed .*by /, '');
+
+        var perfAKA = trackInfo[j].match(/\(as ([^)]*)\)/);
+        if (perfAKA !== null) {
+          performer = perfAKA[1];
+        }
+
+        track.performer = performer.replace(/ \([^)]*\)/, '');
+      } else if (trackInfo[j].match(/.*Written .*by /) !== null) {
+        var writer = trackInfo[j].replace(/.*Written .*by /, '');
+
+        var writeAKA = trackInfo[j].match(/\(as ([^)]*)\)/);
+        if (writeAKA !== null) {
+          writer = writeAKA[1];
+        }
+
+        track.writer = writer.replace(/ \([^)]*\)/, '');
+      }
+    }
+
+    soundtrack.push(track);
+  }
+
+  return soundtrack;
+}
+
 function checkTokenRefreshed(response, api) {
   if (response.error && response.error.statusCode === 401) {
     api.refreshAndUpdateAccessToken();
@@ -131,3 +178,5 @@ function onSpotify(spotifyAPI, song) {
       return false;
     }
  }
+
+function 
